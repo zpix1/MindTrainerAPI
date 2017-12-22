236c3aaea46b8961544e3b7b03bea331e5579daf
Ivan Baksheev <zpix-dev@list.ru>
2017-12-22 18:12:34 +0700

    readme changed

--- trainer/task.cpp
+++ trainer/task.cpp
@@ -13,10 +13,11 @@ int main(int argc, char* argv[]){
     auto data = json::parse(argv[1]);
     // data["mode"].get<string>() - отдай по ключу mode значение типа string
     if (data["mode"].get<string>() == "generate"){
-        int maxval = stoi(data["config"]["maxval1"].get<string>());
+        int maxval1 = stoi(data["config"]["maxval1"].get<string>());
+        int maxval2 = stoi(data["config"]["maxval2"].get<string>());
         srand(stoi(data["seed"].get<string>()));
-        int a = rand() % maxval;
-        int b = rand() % maxval;
+        int a = rand() % maxval1;
+        int b = rand() % maxval2;
         string ans = to_string(a) + " + " + to_string(b) + " = ?";
         json result;
         result["question"] = to_string(a) + " + " + to_string(b) + " = ?";
